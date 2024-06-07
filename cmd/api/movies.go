f93f959bc203a5781eaa6e8e08158ba6dc4bbc74
sergiorock <lopez.sergio.n@gmail.com>
2024-06-07 14:47:26 -0300

    create readJson helper to managing bad request

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -7,10 +7,32 @@ import (
 	"time"
 )
 
-// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
-// return a plain-text placeholder response.
+// Add a createMovieHandler for the "POST /v1/movies" endpoint.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	// Declare an anonymous struct to hold the information that we expect to be in the
+	// HTTP request body (note that the field names and types in the struct are a subset
+	// of the Movie struct that we created earlier).
+	// This struct will be our *target decode destination*.
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// Use the new readJSON() helper to decode the request body into the input struct.
+	// If this returns an error we send the client the error message along with a 400
+	// Bad Request status code, just like before.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		// Use the new badRequestResponse() helper.
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Dump the contents of the input struct in a HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
+
 }
 
 // Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
