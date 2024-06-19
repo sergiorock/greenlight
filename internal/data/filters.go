d4770fa70077e63e1838a260a2a1dbc9066283f1
sergiorock <lopez.sergio.n@gmail.com>
2024-06-19 20:45:55 -0300

    allow sorting by column names on movie table

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"greenlight/internal/validator"
+	"strings" // New import
 )
 
 type Filters struct {
@@ -9,6 +10,29 @@ type Filters struct {
 	PageSize     int
 	Sort         string
 	SortSafelist []string
+}
+
+// Check that the client-provided Sort field matches one of the entries in our safelist
+// and if it does, extract the column name from the Sort field by stripping the leading
+// hyphen character (if one exists).
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+// Return the sort direction ("ASC" or "DESC") depending on the prefix character of the
+// Sort field.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
