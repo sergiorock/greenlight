80f0765c154ad64f31550faa4f44b380a27563cc
sergiorock <lopez.sergio.n@gmail.com>
2024-06-25 14:57:03 -0300

    add global rate limiter

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -28,6 +28,6 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHanlder)
 
-	// Wrap the router with the panic recovery middleware.
-	return app.recoverPanic(router)
+	// Wrap the router with the rateLimit() middleware.
+	return app.recoverPanic(app.rateLimit(router))
 }
