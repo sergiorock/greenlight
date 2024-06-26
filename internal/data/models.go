52fe099bb0b8eb90bc87f75c80869c5481c2a7a0
sergiorock <lopez.sergio.n@gmail.com>
2024-06-26 14:54:36 -0300

    endpoint to create users

--- internal/data/models.go
+++ internal/data/models.go
@@ -14,11 +14,13 @@ var (
 // Create a Models struct which wraps the MovieModel. We'll add other models to this, // like a UserModel and PermissionModel, as our build progresses.
 type Models struct {
 	Movies MovieModel
+	Users  UserModel // Add a new Users field.
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing // the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
