ef407f3eec28c340526d4516a14e847cef4a09b3
sergiorock <lopez.sergio.n@gmail.com>
2024-06-27 15:30:20 -0300

    send welcome email trhough a go rutine

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"errors"
+	"errors" // New import
 	"greenlight/internal/data"
 	"greenlight/internal/validator"
 	"net/http"
@@ -65,15 +65,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	// Call the Send() method on our Mailer, passing in the user's email address,
-	// name of the template file, and the User struct containing the new user's data.
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+	// Use the background helper to execute an anonymous function that sends the welcome
+	// email.
+	app.background(func() {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+		}
+	})
 
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	// Note that we also change this to send the client a 202 Accepted status code.
+	// This status code indicates that the request has been accepted for processing, but
+	// the processing has not been completed.
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
